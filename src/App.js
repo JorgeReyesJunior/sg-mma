404e1627904d47a7ea93b6890de78b52261ebf6c
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-06-07 22:15:40 -0700

    Add 'Header' and 'Carousel' components, to test out 'Banners' carousel.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Carousel from './components/Carousel/Carousel';
+import Banner1 from './components/Banners/Banner1';
+import Banner2 from './components/Banners/Banner2';
+import Banner3 from './components/Banners/Banner3';
 import './App.css';
+
+const slides = [
+  { component: <Banner1 /> },
+  { component: <Banner2 /> },
+  { component: <Banner3 /> }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="o-app">
+      <Header />
+      <main className="o-app__main">
+        <Carousel slides={slides} slideTime={5} />
+      </main>
     </div>
   );
 }
