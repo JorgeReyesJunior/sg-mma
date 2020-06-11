0d63581886cbef7027ffa7a6bdffc910b6742938
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-06-11 11:27:23 -0700

    Update 'Nav' component.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,37 @@
 import React, { createRef } from 'react';
 
-function Nav() {
-  /* TODO: Add animation/ css transition */
-  
+function Nav(props) {
   const dropdownMenu = createRef();
+  const className = () => {
+    return props.navState()
+  }
   
+  /* TODO: Map out page anchors */
   return (
-    <nav className="o-app__nav c-nav c-nav--dropdown" ref={dropdownMenu}>
+    <nav className={className()} ref={dropdownMenu}>
       <ul className="c-nav__list">
-        <li className="c-nav__listing">
+        <li className="c-nav__listing" onClick={props.toggleMenu}>
           <a className="c-nav__anchor" href="#home">Home</a>
         </li>
-        <li className="c-nav__listing">About</li>
-        <li className="c-nav__listing">Curriculum</li>
-        <li className="c-nav__listing">Schedule</li>
-        <li className="c-nav__listing">News + Events</li>
-        <li className="c-nav__listing">Testimonials</li>
-        <li className="c-nav__listing">Shop</li>
-        <button className="c-nav__cta">Free Trial</button>
+        <li className="c-nav__listing" onClick={props.toggleMenu}>
+          <a className="c-nav__anchor" href="#about">About</a>
+        </li>
+        <li className="c-nav__listing" onClick={props.toggleMenu}>
+          <a className="c-nav__anchor" href="#curriculum">Curriculum</a>
+        </li>
+        <li className="c-nav__listing" onClick={props.toggleMenu}>
+          <a className="c-nav__anchor" href="#schedule">Schedule</a>
+        </li>
+        <li className="c-nav__listing" onClick={props.toggleMenu}>
+          <a className="c-nav__anchor" href="#news-and-events">News + Events</a>
+        </li>
+        <li className="c-nav__listing" onClick={props.toggleMenu}>
+          <a className="c-nav__anchor" href="#testimonials">Testimonials</a>
+        </li>
+        <li className="c-nav__listing" onClick={props.toggleMenu}>
+          <a className="c-nav__anchor" href="#shop">Shop</a>
+        </li>
+        <button className="c-nav__cta" onClick={props.toggleMenu}>Free Trial</button>
       </ul>
     </nav>
   )
