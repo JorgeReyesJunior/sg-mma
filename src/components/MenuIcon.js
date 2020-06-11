db98d8ae2b9e1fabe80dc653fc2a209159aca840
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-06-11 11:27:50 -0700

    Update 'MenuIcon' component.

--- src/components/MenuIcon.js
+++ src/components/MenuIcon.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import HamburgerMenuIcon from '../icons/menu-icon.svg';
+import CloseMenuIcon from '../icons/close-icon.svg';
 
 function MenuIcon(props) {
-  return (
-    <svg className="o-app__menu" viewBox="0 0 32 32" onClick={props.toggleMenu}><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
-  )
+  if(props.isOpen) {
+    return (
+      <div className="o-app__menu o-app-menu--is-open" onClick={props.toggleMenu}>
+        <img className="o-app__menu--icon" src={CloseMenuIcon} alt="Hamburger menu." />
+      </div>
+    )
+  } else {
+    return (
+      <div className="o-app__menu" onClick={props.toggleMenu}>
+        <img className="o-app__menu--icon" src={HamburgerMenuIcon} alt="Close hamburger menu." />
+      </div>
+    )
+  }
 }
 
 export default MenuIcon;
