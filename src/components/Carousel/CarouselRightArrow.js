b79a50479c0775b7dbd80283c9a2e1276e5bacf1
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-06-11 11:29:43 -0700

    Update 'Arrow' component.

--- src/components/Carousel/CarouselRightArrow.js
+++ src/components/Carousel/CarouselRightArrow.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import RightArrow from '../../icons/chevron-icon.svg';
 
 function CarouselRightArrow(props) {
   return (
     <button className="c-carousel__arrow c-carousel__arrow--right" onClick={props.onClick}>
-      {`>`}
+      <img className="c-carousel__arrow--icon" src={RightArrow} alt='Carousel left arrow.' />
     </button>
   )
 }
