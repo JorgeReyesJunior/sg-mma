029d9ac5c223d93415fce28ab121ffe065732c48
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-07-22 17:47:06 -0700

    Updates

---
+++ precache-manifest.6990f1ba47dab28e829f6eaa7fb89971.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6e405c952c230281904c05cf9fc388",
+    "url": "/sg-mma/index.html"
+  },
+  {
+    "revision": "ee892eec7db2ad21decb",
+    "url": "/sg-mma/static/css/main.8c089075.chunk.css"
+  },
+  {
+    "revision": "adb9abaa5e7d05a88363",
+    "url": "/sg-mma/static/js/2.964c4723.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sg-mma/static/js/2.964c4723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee892eec7db2ad21decb",
+    "url": "/sg-mma/static/js/main.b32bb093.chunk.js"
+  },
+  {
+    "revision": "2858cb8956dfade80ae3",
+    "url": "/sg-mma/static/js/runtime-main.c00b4635.js"
+  },
+  {
+    "revision": "4140ff2ec71a64c22287f0df158e4223",
+    "url": "/sg-mma/static/media/chevron-icon.4140ff2e.svg"
+  },
+  {
+    "revision": "40e9c520cb17832378f11c21404261a6",
+    "url": "/sg-mma/static/media/close-icon.40e9c520.svg"
+  },
+  {
+    "revision": "ab3ed4c8b0d9b904387f92561e0235ce",
+    "url": "/sg-mma/static/media/logo-v2.ab3ed4c8.png"
+  },
+  {
+    "revision": "1621cf60b0c50811cf1f6a75f4229e19",
+    "url": "/sg-mma/static/media/map-pin-icon.1621cf60.svg"
+  },
+  {
+    "revision": "cfce727ac11755596a5d814523c1661c",
+    "url": "/sg-mma/static/media/menu-icon.cfce727a.svg"
+  },
+  {
+    "revision": "700c17ea94ccc1bd27607bfea999d946",
+    "url": "/sg-mma/static/media/paper-note-icon.700c17ea.svg"
+  }
+]);
