40e6ae44ef86588c9c299d357579e42dd5a63051
J. Reyes Jr <reyes.junior.27@gmail.com>
2020-07-22 17:25:49 -0700

    Update course 'startTime' and 'endTime' values.

--- src/assets/data/ScheduleMap.js
+++ src/assets/data/ScheduleMap.js
@@ -3,158 +3,158 @@ const ScheduleMap = new Map();
 const Monday = [
   {
     name: 'Adult BJJ Fundamentals',
-    startTime: '6:00 - AM',
-    endTime: '7:00 - AM'
+    startTime: '6:00 AM',
+    endTime: '7:00 AM'
   },
   {
     name: 'Adult BJJ Advanced',
-    startTime: '12:00 - PM',
-    endTime: '1:00 - PM'
+    startTime: '12:00 PM',
+    endTime: '1:00 PM'
   },
   {
     name: 'Kids BJJ Fundamentals',
-    startTime: '5:30 - PM',
-    endTime: '6:30 - PM'
+    startTime: '5:30 PM',
+    endTime: '6:30 PM'
   },
   {
     name: 'Yoga',
-    startTime: '6:30 - PM',
-    endTime: '7:30 - PM'
+    startTime: '6:30 PM',
+    endTime: '7:30 PM'
   },
   {
     name: 'Adult BJJ Advanced',
-    startTime: '7:30 - PM',
-    endTime: '8:30 - PM'
+    startTime: '7:30 PM',
+    endTime: '8:30 PM'
   },
   {
     name: 'Adult BJJ Fundamentals',
-    startTime: '8:30 - PM',
-    endTime: '9:30 - PM'
+    startTime: '8:30 PM',
+    endTime: '9:30 PM'
   }
 ];
 
 const Tuesday = [
   {
     name: 'Adult BJJ Advanced',
-    startTime: '12:00 - PM',
-    endTime: '1:00 - PM'
+    startTime: '12:00 PM',
+    endTime: '1:00 PM'
   },
   {
     name: 'Yoga',
-    startTime: '6:30 - PM',
-    endTime: '7:30 - PM'
+    startTime: '6:30 PM',
+    endTime: '7:30 PM'
   },
   {
     name: 'Adult BJJ Advanced',
-    startTime: '7:30 - PM',
-    endTime: '8:30 - PM'
+    startTime: '7:30 PM',
+    endTime: '8:30 PM'
   },
   {
     name: 'Adult BJJ Fundamentals',
-    startTime: '8:30 - PM',
-    endTime: '9:30 - PM'
+    startTime: '8:30 PM',
+    endTime: '9:30 PM'
   }
 ];
 
 const Wednesday = [
   {
     name: 'Adult BJJ Fundamentals',
-    startTime: '6:00 - AM',
-    endTime: '7:00 - AM'
+    startTime: '6:00 AM',
+    endTime: '7:00 AM'
   },
   {
     name: 'Adult BJJ Advanced',
-    startTime: '12:00 - PM',
-    endTime: '1:00 - PM'
+    startTime: '12:00 PM',
+    endTime: '1:00 PM'
   },
   {
     name: 'Kids BJJ Fundamentals',
-    startTime: '5:30 - PM',
-    endTime: '6:30 - PM'
+    startTime: '5:30 PM',
+    endTime: '6:30 PM'
   },
   {
     name: 'Yoga',
-    startTime: '6:30 - PM',
-    endTime: '7:30 - PM'
+    startTime: '6:30 PM',
+    endTime: '7:30 PM'
   },
   {
     name: 'Adult BJJ Advanced',
-    startTime: '7:30 - PM',
-    endTime: '8:30 - PM'
+    startTime: '7:30 PM',
+    endTime: '8:30 PM'
   },
   {
     name: 'Adult BJJ Fundamentals',
-    startTime: '8:30 - PM',
-    endTime: '9:30 - PM'
+    startTime: '8:30 PM',
+    endTime: '9:30 PM'
   }
 ];
 
 const Thursday = [
   {
     name: 'Adult BJJ Advanced',
-    startTime: '12:00 - PM',
-    endTime: '1:00 - PM'
+    startTime: '12:00 PM',
+    endTime: '1:00 PM'
   },
   {
     name: 'Yoga',
-    startTime: '6:30 - PM',
-    endTime: '7:30 - PM'
+    startTime: '6:30 PM',
+    endTime: '7:30 PM'
   },
   {
     name: 'Adult BJJ Advanced',
-    startTime: '7:30 - PM',
-    endTime: '8:30 - PM'
+    startTime: '7:30 PM',
+    endTime: '8:30 PM'
   },
   {
     name: 'Adult BJJ Fundamentals',
-    startTime: '8:30 - PM',
-    endTime: '9:30 - PM'
+    startTime: '8:30 PM',
+    endTime: '9:30 PM'
   }
 ];
 
 const Friday = [
   {
     name: 'Adult BJJ Fundamentals',
-    startTime: '6:00 - AM',
-    endTime: '7:00 - AM'
+    startTime: '6:00 AM',
+    endTime: '7:00 AM'
   },
   {
     name: 'Adult BJJ Advanced',
-    startTime: '12:00 - PM',
-    endTime: '1:00 - PM'
+    startTime: '12:00 PM',
+    endTime: '1:00 PM'
   },
   {
     name: 'Kids BJJ Fundamentals',
-    startTime: '5:30 - PM',
-    endTime: '6:30 - PM'
+    startTime: '5:30 PM',
+    endTime: '6:30 PM'
   },
   {
     name: 'Yoga',
-    startTime: '6:30 - PM',
-    endTime: '7:30 - PM'
+    startTime: '6:30 PM',
+    endTime: '7:30 PM'
   },
   {
     name: 'Adult BJJ Advanced',
-    startTime: '7:30 - PM',
-    endTime: '8:30 - PM'
+    startTime: '7:30 PM',
+    endTime: '8:30 PM'
   },
   {
     name: 'Adult BJJ Fundamentals',
-    startTime: '8:30 - PM',
-    endTime: '9:30 - PM'
+    startTime: '8:30 PM',
+    endTime: '9:30 PM'
   }
 ];
 
 const Saturday = [
   {
     name: 'Adult BJJ Fundamentals',
-    startTime: '7:30 - AM',
-    endTime: '8:30 - AM'
+    startTime: '7:30 AM',
+    endTime: '8:30 AM'
   },
   {
     name: 'Adult BJJ Advanced',
-    startTime: '9:30 - PM',
-    endTime: '10:30 - PM'
+    startTime: '9:30 PM',
+    endTime: '10:30 PM'
   }
 ];
 
